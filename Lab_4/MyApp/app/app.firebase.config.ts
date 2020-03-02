0aebeff43e6c3ad610a4b2b7cb5b6c8a50834667
Alshaikh1abbas <54451872+Alshaikh1abbas@users.noreply.github.com>
2020-03-01 21:44:18 -0600

    delete

--- Lab_4/MyApp/app/app.firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG =  {
-  apiKey: "AIzaSyCLSmTEPyPd8lKsjGC2xtVOPgUq5fHDpPs",
-  authDomain: "asemax226.firebaseapp.com",
-  databaseURL: "https://asemax226.firebaseio.com",
-  projectId: "asemax226",
-  storageBucket: "asemax226.appspot.com",
-  messagingSenderId: "489123086268"
-};
--- Lab_4/MyApp/services/rest/GoogleCloudVision.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  googleCloudVisionAPIKey: "AIzaSyBM-8OAGOsrPY5mSPqQGdkJEW5AF2Ts5KY"
-  // Using the browser key
-};
